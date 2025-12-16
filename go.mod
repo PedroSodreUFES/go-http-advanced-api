8fd18cb70da5b57ce653c34d491ee8a152faf75f
PedroSodreUFES <pedro.malini@edu.ufes.br>
2025-12-16 13:42:39 -0300

    feat: hot reload + tern + docker + layer pattern

---
+++ go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.22.2
+
+require (
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/joho/godotenv v1.5.1
+)
---
+++ internal/api/handlers.go
@@ -0,0 +1,15 @@
+package api
+
+import "net/http"
+
+func (api *Api) HandleSignupUser(w http.ResponseWriter, r *http.Request) {
+	panic("TODO")
+}
+
+func (api *Api) handleLoginUser(w http.ResponseWriter, r *http.Request) {
+	panic("TODO")
+}
+
+func (api *Api) handleLogoutUser(w http.ResponseWriter, r *http.Request) {
+	panic("TODO")
+}	
